70f8a477dec887546baa699f1067f5528952b114
lauPattes <131869236+lauPattes@users.noreply.github.com>
2023-11-13 11:17:51 +0000

    lesson 4.3 complete

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product.module';
 
 @Component({
   selector: 'bot-catalog',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./catalog.component.css']
 })
 export class CatalogComponent {
+  product: IProduct;
 
+  constructor(){
+    this.product = {
+      id: 2,
+      description: "A friendly robot head with two eyes and a smile -- great for domestic use.",
+      name: "Friendly Bot",
+      imageName: "head-friendly.png",
+      category: "Heads",
+      price: 945.0,
+      disount: 0.2, 
+    };
+  }
 }
